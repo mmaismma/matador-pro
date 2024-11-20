8065ec95b8e6ea6ac21794c1d97d7527fe5224c2
mmaismma <mmaismma@hotmail.com>
2024-11-20 14:45:55 +0530

    Refactor project structure, cleanup files

---
+++ src/main/java/app/MatadorPro.java
@@ -0,0 +1,11 @@
+package app;
+
+public class MatadorPro {
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            javax.swing.JFrame f = new ui.LandingPage();
+            f.setVisible(true);
+        });
+    }
+}
---
+++ src/main/java/resources/GetResource.java
@@ -0,0 +1,7 @@
+package resources;
+
+public class GetResource {
+    public GetResource(String url) {
+        this.getClass().getResource(url);
+    }
+}
