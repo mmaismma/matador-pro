c45467b94ba26222cfe96cb73254ab441ba9b179
rockm <rockm@Mahesh_Sharma>
2024-11-24 00:33:05 +0530

    1) help page 2) seat selection frame (seat map) 3) INR amount of seat(s) 4) View ticket + pay at counter 5) complain registration about app
    These UIs are completed.

--- src/main/resources/data-dump.sql
+++ src/main/resources/data-dump.sql
@@ -1,20 +1,19 @@
-/*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.5.2-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: localhost    Database: matadorpro
 -- ------------------------------------------------------
--- Server version	11.5.2-MariaDB
+-- Server version	9.1.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `users`
@@ -22,16 +21,16 @@
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `mobile` varchar(10) DEFAULT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `gender` varchar(50) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `dob` varchar(10) DEFAULT NULL,
-  `otp` varchar(6) DEFAULT NULL,
-  `user_id` varchar(6) DEFAULT NULL
+  `mobile` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `first_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dob` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `otp` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_id` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -41,10 +40,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES
-('7488446379','Mayank','Arya','Male','mmaismma@hotmail.com','26-11-2004','123456','NnEOwm'),
-('9588526275','Rahul','Malhotra','Male','malhotra.rahulggn06@gmail.com','06-04-2004','876192','H2Gr5T'),
-('7073045837','Mahesh','Sharma','Male','22bcs051@smvdu.ac.in','26-11-2004','567890','pP9CZc');
+INSERT INTO `users` VALUES ('7488446379','Mayank','Arya','Male','mmaismma@hotmail.com','26-11-2004','123456','NnEOwm'),('9588526275','Rahul','Malhotra','Male','malhotra.rahulggn06@gmail.com','06-04-2004','876192','H2Gr5T'),('7073045837','Mahesh','Sharma','Male','22bcs051@smvdu.ac.in','26-11-2004','567890','pP9CZc');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,6 +51,6 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-22 14:45:32
+-- Dump completed on 2024-11-22 18:38:03
