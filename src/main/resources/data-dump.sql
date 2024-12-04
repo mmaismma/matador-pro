3a6217ebb343b65c61af157cd3ad96d4a44b4ac1
Mayank Arya <mmaismma@hotmail.com>
2024-12-05 00:51:53 +0530

    data dump

--- src/main/resources/data-dump.sql
+++ src/main/resources/data-dump.sql
@@ -1,19 +1,182 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.5.2-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: matadorpro
 -- ------------------------------------------------------
--- Server version	9.1.0
+-- Server version	11.5.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `agents`
+--
+USE matadorpro;
+
+DROP TABLE IF EXISTS `agents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `agents` (
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `aadhar` varchar(100) DEFAULT NULL,
+  `mobile` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `otp` varchar(100) DEFAULT NULL,
+  `id` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agents`
+--
+
+LOCK TABLES `agents` WRITE;
+/*!40000 ALTER TABLE `agents` DISABLE KEYS */;
+INSERT INTO `agents` VALUES
+('Agent','Vinod','6234578','1234567890','agent.vinod@gmail.com','234567','MDS3xY');
+/*!40000 ALTER TABLE `agents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bookings`
+--
+
+DROP TABLE IF EXISTS `bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bookings` (
+  `booking_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(6) DEFAULT NULL,
+  `bus_number` varchar(100) DEFAULT NULL,
+  `schedule_id` varchar(10) DEFAULT NULL,
+  `stop_from` varchar(100) DEFAULT NULL,
+  `stop_to` varchar(100) DEFAULT NULL,
+  `booking_time` timestamp NULL DEFAULT current_timestamp(),
+  `seat_type` varchar(50) DEFAULT NULL,
+  `fare` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`booking_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookings`
+--
+
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+INSERT INTO `bookings` VALUES
+(1,'NnEOwm','28392','PXJCc8','SMVDU G1','SMVDU G2','2024-12-04 04:15:38','standing','70'),
+(2,'NnEOwm','28392','PXJCc8','SMVDU G1','SMVDU G2','2024-12-04 09:33:03','sitting','120');
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buses`
+--
+
+DROP TABLE IF EXISTS `buses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `buses` (
+  `name` varchar(100) DEFAULT NULL,
+  `number` varchar(100) DEFAULT NULL,
+  `agent` varchar(100) DEFAULT NULL,
+  `standing_fare` varchar(100) DEFAULT NULL,
+  `sitting_fare` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buses`
+--
+
+LOCK TABLES `buses` WRITE;
+/*!40000 ALTER TABLE `buses` DISABLE KEYS */;
+INSERT INTO `buses` VALUES
+('KATRA TO DOMAIL','1245','MDS3xY','25','70'),
+('DOMAIL TO UDHAMPUR','28392','MDS3xY','70','120'),
+('PANTHAL TO KATRA','JK78AJ1829','MDS3xY','20','30');
+/*!40000 ALTER TABLE `buses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schedules`
+--
+
+DROP TABLE IF EXISTS `schedules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schedules` (
+  `schedule_id` varchar(10) DEFAULT NULL,
+  `days` varchar(7) DEFAULT NULL,
+  `bus_number` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedules`
+--
+
+LOCK TABLES `schedules` WRITE;
+/*!40000 ALTER TABLE `schedules` DISABLE KEYS */;
+INSERT INTO `schedules` VALUES
+('fOXBkJ','1111100','1245'),
+('efSukb','0000011','1245'),
+('PXJCc8','0111110','28392'),
+('GyKr9o','1111100','JK78AJ1829'),
+('YqKzgS','0000011','JK78AJ1829');
+/*!40000 ALTER TABLE `schedules` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stops`
+--
+
+DROP TABLE IF EXISTS `stops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stops` (
+  `schedule_id` varchar(10) DEFAULT NULL,
+  `stop_name` varchar(100) DEFAULT NULL,
+  `stop_sequence` int(11) DEFAULT NULL,
+  `arrival` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stops`
+--
+
+LOCK TABLES `stops` WRITE;
+/*!40000 ALTER TABLE `stops` DISABLE KEYS */;
+INSERT INTO `stops` VALUES
+('fOXBkJ','Katra',1,'8:00'),
+('fOXBkJ','Panthal',2,'8:20'),
+('fOXBkJ','SMVDU',3,'8:27'),
+('fOXBkJ','Domail',4,'8:45'),
+('efSukb','Katra',1,'10:00'),
+('efSukb','Panthal',2,'10:30'),
+('efSukb','SMVDU',3,'10:40'),
+('efSukb','Domail',4,'11:10'),
+('PXJCc8','Domail',1,'13:00'),
+('PXJCc8','SMVDU G1',2,'13:20'),
+('PXJCc8','SMVDU G2',3,'13:25'),
+('PXJCc8','Udhmapur',4,'15:25'),
+('GyKr9o','Panthal',1,'9:00'),
+('GyKr9o','Katra',2,'9:20'),
+('YqKzgS','Panthal',1,'11:00'),
+('YqKzgS','Katra',2,'11:20');
+/*!40000 ALTER TABLE `stops` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -21,16 +184,16 @@
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `mobile` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `first_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `gender` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `dob` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `otp` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_id` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `mobile` varchar(10) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `dob` varchar(10) DEFAULT NULL,
+  `otp` varchar(6) DEFAULT NULL,
+  `user_id` varchar(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -40,7 +203,12 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('7488446379','Mayank','Arya','Male','mmaismma@hotmail.com','26-11-2004','123456','NnEOwm'),('9588526275','Rahul','Malhotra','Male','malhotra.rahulggn06@gmail.com','06-04-2004','876192','H2Gr5T'),('7073045837','Mahesh','Sharma','Male','22bcs051@smvdu.ac.in','26-11-2004','567890','pP9CZc');
+INSERT INTO `users` VALUES
+('7488446379','Mayank','Arya','Male','mmaismma@hotmail.com','26-07-2004','123456','NnEOwm'),
+('9588526275','Rahul','Malhotra','Male','malhotra.rahulggn06@gmail.com','06-04-2004','876192','H2Gr5T'),
+('7073045837','Mahesh','Sharma','Male','22bcs051@smvdu.ac.in','26-11-2004','567890','pP9CZc'),
+('1234567890','Kaushal','Gutpa','Male','22bcs041@smvdu.ac.in','12-12-2004','789456','2DMJ5r'),
+('9122234567','Anhad','Mahajan','Female','anhad.sharma@gmail.com','26-11-2005','123567','d7L7OC');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,6 +219,6 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2024-11-22 18:38:03
+-- Dump completed on 2024-12-05  0:51:14
